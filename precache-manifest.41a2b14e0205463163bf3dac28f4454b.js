2faba5ab8252314bd6b95488642836fb457c9b7e
badinenisaivardhan <badinenisaivardhan@gmail.com>
2020-04-21 16:56:12 +0530

    Updates

---
+++ precache-manifest.41a2b14e0205463163bf3dac28f4454b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fff7444bd212eb60f38bf9f209c7f9af",
+    "url": "/covid-19/index.html"
+  },
+  {
+    "revision": "5ac6e11a70ae0ec9607e",
+    "url": "/covid-19/static/css/main.39d4cc1f.chunk.css"
+  },
+  {
+    "revision": "e6478f9353e4c4ea5c28",
+    "url": "/covid-19/static/js/2.1b626b89.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/covid-19/static/js/2.1b626b89.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ac6e11a70ae0ec9607e",
+    "url": "/covid-19/static/js/main.375f370d.chunk.js"
+  },
+  {
+    "revision": "58ccbf4ac62906e56f32",
+    "url": "/covid-19/static/js/runtime-main.0a05f1ba.js"
+  }
+]);
